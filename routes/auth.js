e7ff6b1487ad92cc4f559206b1413ff9b365fec6
Jonathan Bruckman <96599491+Makomanix@users.noreply.github.com>
2025-02-11 17:11:07 -0500

    added validation checks to signup route

--- routes/auth.js
+++ routes/auth.js
@@ -4,9 +4,29 @@ import { body, check } from 'express-validator';
 const router = express.Router();
 
 router.put('/signup', [
+body('email')
+  .isEmail()
+  .withMessage('Please enter a valid email.')
+  .custom((value, { req }) => {
+    return User.findOne({ email: value }).then(userDoc => {
+      if (userDoc) {
+        return Promise.reject('E-mail address already exists!');
+      }
+    });
+  })
+  .normalizeEmail(),
+body('password')
+  .trim()
+  .isLength({min: 7})
+  .matches(/^(?=.*[!@#$%^&*])/)
+  .withMessage('Minimum length 7 and must include 1 special character.'),
+body('username')
+  .trim()
+  .not()
+  .isEmpty()
+]
+, authController.signup);
 
-]);
-
-router.post('/login', );
+router.post('/login', authController.login );
 
 export default router;
